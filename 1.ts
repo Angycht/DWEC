77fe8a79c9b93e1dcf531e70656805398cab481b
Angycht <angycht@gmail.com>
2024-10-25 08:36:56 +0200

    Boletin DWEC

---
+++ 1.ts
@@ -0,0 +1,17 @@
+/*
+Realiza un programa que haciendo uso del objeto DATE, crees un objeto de una
+para saludar al usuario tras abrir la aplicación indicando la hora y la fecha actual.
+Haz también que se muestre por consola una hora posterior y una hora anterior.
+*/
+var meses :string[]=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+var  fechaActual :  Date =  new  Date ();
+
+window.alert("Hola, la fecha actual es: " + fechaActual.getDate()+ "/"+(meses[fechaActual.getMonth()]
+)+"/"+fechaActual.getFullYear());
+
+console.log('La hora de antes es '+ (fechaActual.getHours()-1) + ':' +fechaActual.getMinutes() + ':' + fechaActual.getSeconds() );
+
+console.log('La hora de despues es '+ (fechaActual.getHours()+1) + ':' +fechaActual.getMinutes() + ':' + fechaActual.getSeconds() );
+
+
+
---
+++ 3.ts
@@ -0,0 +1,6 @@
+/*Escribe un programa que le pida al usuario qué porcentaje quiere aplicar para
+redimensionar la ventana. El programa debe pedir confirmación y solo en caso de
+que se acepte, se redimensionará la ventana al porcentaje indicado. Es necesario
+igualmente ir mostrando por consola que va ocurriendo en cada momento. ¿Se
+puede hacer esto? ¿Por qué crees que puede ocurrir?*/
+
